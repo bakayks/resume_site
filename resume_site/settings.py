2f8942c7cc20e227c4e62e8b59122b5a122fa05d
bakai <bakaiks312@gmail.com>
2025-03-23 18:25:22 +0600

    script refreshed

--- resume_site/settings.py
+++ resume_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-s%t=hdi)=6nsx-cr-d^ydtgutfp!!emz_gd$rdp9%5inxdi1t0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
